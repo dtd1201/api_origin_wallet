96d02c6e9409c2a56184d756c42db80d239e6e64
dtd1201 <datlua01@gmail.com>
2026-09-07 13:27:08 +0700

    update br number v4

--- app/Services/Nium/NiumCustomerPayloadFactory.php
+++ app/Services/Nium/NiumCustomerPayloadFactory.php
@@ -311,12 +311,14 @@ class NiumCustomerPayloadFactory
 
         return $this->filter([
             'corporate' => [
-                'businessName' => $profile->business_name,
-                'registeredCountry' => strtoupper((string) $profile->registered_country_code),
-                'businessRegistrationNumber' => $profile->business_registration_number,
-                'positions' => [
-                    [
-                        'title' => 'director',
+                [
+                    'businessName' => $profile->business_name,
+                    'registeredCountry' => strtoupper((string) $profile->registered_country_code),
+                    'businessRegistrationNumber' => $profile->business_registration_number,
+                    'positions' => [
+                        [
+                            'title' => 'director',
+                        ],
                     ],
                 ],
             ],
