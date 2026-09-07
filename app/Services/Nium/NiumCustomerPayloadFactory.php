fe8e874ad588ede3284c97135c7d6146e3724a60
dtd1201 <datlua01@gmail.com>
2026-09-07 15:02:47 +0700

    update iso

--- app/Services/Nium/NiumCustomerPayloadFactory.php
+++ app/Services/Nium/NiumCustomerPayloadFactory.php
@@ -174,6 +174,7 @@ class NiumCustomerPayloadFactory
             true,
             normalizeNiumPositions: $region === 'SG',
             positionsOverride: $hkApplicantPositions,
+            'applicant'
         );
 
         $payload = $this->filter(array_merge($this->regionFields($profile, $region), [
@@ -311,6 +312,7 @@ class NiumCustomerPayloadFactory
                     'nium_v5_fields.stakeholders.individual[*].email',
                     false,
                     $normalizeNiumPositions,
+                    'stakeholder',
                 );
             })
             ->values()
